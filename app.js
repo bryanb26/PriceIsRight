c07dba13e5c8f1ae3b51eb27d70b6b0ab1600ba3
bryanb26 <50746711+bryanb26@users.noreply.github.com>
2019-12-09 11:46:18 -0500

    Actual Work

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-async function productCall() {
+/*  async function productCall() {
     try {
-      let data = await fetch(`https://api.bestbuy.com/v1/products/8880044.json?show=sku,name,salePrice&apiKey=pGkFlneZilafA6UKWELwcNc3`);
+      let data = await fetch(`https://api.bestbuy.com/v1/products/8880044.json?show=sku,name,salePrice&apiKey=pGkFlneZilafA6UKWELwcNc3`, {
+        method: 'GET',
+        body: JSON.stringify(data);
+      });
       console.log(data);
       return data = products
     } catch (err) {
@@ -8,4 +11,24 @@ async function productCall() {
     }
   }
 
-  productCall();
+  productCall();  */
+  var request = require('request');
+
+  var options = {
+    method: 'GET',
+    url: `https://api.bestbuy.com/v1/products/${randomSku(01000000,10000000)}.json?show=sku,name,salePrice&apiKey=pGkFlneZilafA6UKWELwcNc3`,
+    json: true
+    
+  };
+
+  request(options, function(err, res, body) {
+    console.log(body.sku)
+  })
+
+  
+
+  function randomSku(min, max) { 
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+console.log(randomSku(01000000,10000000));
