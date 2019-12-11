b6dcd4bbfdcebf85a9b730f9ccefb6fcb997119d
bryanb26 <50746711+bryanb26@users.noreply.github.com>
2019-12-10 22:42:09 -0500

    big update

--- app.js
+++ app.js
@@ -12,13 +12,15 @@
   }
 
   productCall();  */
-var request = require('request');
+//var request = require('request');
 
 var skuArray = [6316142,5721014,6191401,6261177,6255368,5353902,6355044,5706633,6302020,6355731];
 
-let randSku = skuArray[Math.floor(Math.random() * skuArray.length)]
+let randSku = skuArray[Math.floor(Math.random() * skuArray.length)];
 
-var options = {
+let randNumber = Math.floor(Math.random() * 150) + 1;
+
+/* var options = {
     method: 'GET',
     url: `https://api.bestbuy.com/v1/products/${randSku}.json?show=sku,name,salePrice&apiKey=pGkFlneZilafA6UKWELwcNc3`,
     json: true 
@@ -26,11 +28,18 @@ var options = {
 };
 
 request(options, function(err, res, body) {
-    return body.name;
+  return body.name;
+}); */
+
+async function apiCall(){
+  let req = await fetch(`https://api.bestbuy.com/v1/products/${randSku}.json?show=sku,name,salePrice&apiKey=pGkFlneZilafA6UKWELwcNc3`);
+  return req.json()
+};
+
+
+let promise = apiCall(randSku);
+
+promise.then(function(data) {
+  document.querySelector('.question').innerHTML = `Is the price of ${data.name} greater or less than $${randNumber}?`;
 })
-
-
-
-
-
 
