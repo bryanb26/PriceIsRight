d3866dcfe7bbe9fa7410f607f9aef6aa4ff27ed7
bryanb26 <50746711+bryanb26@users.noreply.github.com>
2019-12-11 11:57:06 -0500

    nice

--- app.js
+++ app.js
@@ -14,38 +14,52 @@
   productCall();  */
 //var request = require('request');
 
-var skuArray = [6316142,5721014,6191401,6261177,6255368,5353902,6355044,5706633,6302020,6355731];
+let skuArray = [6316142,5721014,6191401,6261177,6255368,5353902,6355044,5706633,6302020,6355731];
 
 let randSku = skuArray[Math.floor(Math.random() * skuArray.length)];
 
 let randNumber = Math.floor(Math.random() * 150) + 1;
-
-/* var options = {
-    method: 'GET',
-    url: `https://api.bestbuy.com/v1/products/${randSku}.json?show=sku,name,salePrice&apiKey=pGkFlneZilafA6UKWELwcNc3`,
-    json: true 
-    
-};
-
-request(options, function(err, res, body) {
-  return body.name;
-}); */
 
 async function apiCall(){
   let req = await fetch(`https://api.bestbuy.com/v1/products/${randSku}.json?show=sku,name,salePrice&apiKey=pGkFlneZilafA6UKWELwcNc3`);
   return req.json()
 };
 
+let actualValue;
 
-let promise = apiCall(randSku);
-
-promise.then(function(data) {
-  document.querySelector('.question').innerHTML = `Is the price of ${data.name} greater or less than $${randNumber}?`;
-  if (data.salePrice > randNumber){
-    higher = true;
+let promise = apiCall(randSku)
+promise.then(async function(data) {
+  document.querySelector('.question').innerHTML = `Is the price of ${data.name} higher or lower than $${randNumber}?`;
+  console.log(data.salePrice)
+  if(data.salePrice > randNumber){
+    actualValue = "Higher";
+  }else{
+    actualValue = "Lower";
   }
-  else{
-    lower = true;
-  }
+  console.log(actualValue)
+}).then(async x => {
+  document.querySelectorAll('.choice').forEach(x => {
+    x.addEventListener('click', e => {
+      console.log(e.target.innerText)
+      if (e.target.innerText == actualValue) {
+        console.log('correct');
+      }else{
+        console.log('wrong');
+      }
+    })
+  })
 })
 
+
+
+
+function clickListener(el, run){
+  document.querySelectorAll(el).forEach(el => {
+     el.addEventListener('click', run); 
+  });
+};
+
+clickListener(`.choice`,(el)=>{
+  let value = el.target.textContent;
+  console.log(value);
+});
