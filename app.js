022365289c8140f6510b9918d03f1c169bf8884b
bryanb26 <50746711+bryanb26@users.noreply.github.com>
2019-12-11 21:57:03 -0500

    near final

--- app.js
+++ app.js
@@ -16,19 +16,24 @@
 
 let skuArray = [6316142,5721014,6191401,6261177,6255368,5353902,6355044,5706633,6302020,6355731];
 
-let randSku = skuArray[Math.floor(Math.random() * skuArray.length)];
+let randSku;
 
-let randNumber = Math.floor(Math.random() * 150) + 1;
+let randNumber;
 
 async function apiCall(){
+  randSku = skuArray[Math.floor(Math.random() * skuArray.length)];
+  randNumber = Math.floor(Math.random() * 150) + 1;
   let req = await fetch(`https://api.bestbuy.com/v1/products/${randSku}.json?show=sku,name,salePrice&apiKey=pGkFlneZilafA6UKWELwcNc3`);
-  return req.json()
+  return await req.json()
 };
+
+console.log(apiCall().then(async x => {
+  console.log(x)
+}))
 
 let actualValue;
 
-let promise = apiCall(randSku)
-promise.then(async function(data) {
+async function createQuestion(data) {
   document.querySelector('.question').innerHTML = `Is the price of ${data.name} higher or lower than $${randNumber}?`;
   console.log(data.salePrice)
   if(data.salePrice > randNumber){
@@ -37,22 +42,32 @@ promise.then(async function(data) {
     actualValue = "Lower";
   }
   console.log(actualValue)
-}).then(async x => {
-  document.querySelectorAll('.choice').forEach(x => {
-    x.addEventListener('click', e => {
-      console.log(e.target.innerText)
-      if (e.target.innerText == actualValue) {
-        alert("Correct!");
-      }else{
-        alert("Wrong!");
-      }
+}
+function site() {
+  let promise = apiCall()
+  promise.then(x => {createQuestion(x)}).then(async x => {
+    document.querySelectorAll('.choice').forEach(x => {
+      x.addEventListener('click', e => {
+        console.log(e.target.innerText)
+        if (e.target.innerText == actualValue) {
+          alert("Correct!");
+        }else{
+          alert("Wrong!");
+        }
+        let promise = apiCall().then(x => {
+          createQuestion(x)
+        })
+        
+      })
     })
-  })
-});
+  });
+}
+site()
+
 
 function clickListener(el, run){
   document.querySelectorAll(el).forEach(el => {
-     el.addEventListener('click', run); 
+    el.addEventListener('click', run); 
   });
 };
 
